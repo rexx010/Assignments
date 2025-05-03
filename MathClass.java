bc6067ae4283fddf7fdaec7b1638685df47eec3a
rexx010 <grexx200@gmail.com>
2025-05-03 17:07:13 +0100

    LargestAndSmallest using Math class

---
+++ MathClass.java
@@ -0,0 +1,15 @@
+public class MathClass{
+public static void main(String[] args){
+
+int a = 80;
+int b = 75;
+int c = 12;
+int d = 20;
+
+int max =(int) Math.max(a,b);
+int min =(int) Math.min(a,c);
+
+System.out.println(max);
+System.out.println(min);
+}
+}
