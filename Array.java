ee74a4106c9578d3203bf09b7e5cf2b2236ed01b
rexx010 <grexx200@gmail.com>
2025-05-05 13:00:23 +0100

    first commit

---
+++ Array.java
@@ -0,0 +1,24 @@
+public class Array{
+public static void main(String[] args){
+
+int [] number = new int [5];
+
+number[0] = 22;
+number[1] = 51;
+number[2] = 38;
+number[3] = 23;
+number[4] = 48;
+
+for(int counter = 0; counter < number.length; counter++){
+System.out.print(number[counter]+" ");
+}
+System.out.println();
+System.out.println();
+
+for(int counter = 0; counter < number.length; counter++){
+System.out.println(number[counter]+" ");
+}
+
+
+}
+}
