4414d251082e730119331233a876d7f7e2cf354f
rexx010 <grexx200@gmail.com>
2025-05-19 19:26:51 +0100

    practice

---
+++ OOO.java
@@ -0,0 +1,16 @@
+public class OOO{
+public static void main(String[] args){
+
+int[] file = new int[5];
+
+file[0] = 9;
+file[1] = 2;
+file[2] = 4;
+file[3] = 3;
+file[4] = 5;
+
+for(int i = 0; i < file.length - 1; i++) {
+System.out.print(file[i]);
+}
+}
+}
