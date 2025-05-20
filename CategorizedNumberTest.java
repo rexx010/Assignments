53e8affc918e70fa3889d6a3b965ba63af812790
rexx010 <grexx200@gmail.com>
2025-05-20 10:46:49 +0100

    created a test to check for divisor condition

---
+++ CategorizedNumberTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CategorizedNumberTest{
+
+@Test
+public void checkDivideable(){
+int[] categories = {10, 15, 21, 30};
+int[] expected = {10, 15, 30};
+int[] output = CategorizedNumberFunction.divisible(divisible);
+assertArrayEquals(expected, output);
+
+
+}
+
+
+
+
+
+
+
+
+
+}
