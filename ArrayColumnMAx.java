69593f6190be6b534995758e775f8ac0785b9e08
rexx010 <grexx200@gmail.com>
2025-06-01 04:46:06 +0100

    modified the max in 2d

--- ArrayColumnMAx.java
+++ ArrayColumnMAx.java
@@ -1,14 +1,19 @@
 public class ArrayColumnMAx{
 	public static void main(String[] args){
 
-	int[] value = {6,4,2,7,9};
+	int[][] value = {
+			{6,4,2,7,9},
+			{21, 1},
+			{22,3,10,13,12}
+			};
 
-	int max = value[0];
+	int max = value[0][0];
 
 	for(int count = 1; count < value.length; count++){
-		if(max < value[count])max = value[count];
+	for(int j = 0; j < value[count].length; j++){
+		if(max < value[count][j])max = value[count][j];
 	}
-
+	}
 System.out.print(max);
 	}
 }
--- ArrayColumnMin.java
+++ ArrayColumnMin.java
@@ -1,14 +1,19 @@
-public class ArrayColumnMin{
+public class ArrayColumnMAx{
 	public static void main(String[] args){
 
-	int[] value = {6,4,2,7,9};
+	int[][] value = {
+			{6,4,2,7,9},
+			{21, 1},
+			{22,3,10,13,12}
+			};
 
-	int min = value[0];
+	int min = value[0][0];
 
 	for(int count = 1; count < value.length; count++){
-		if(min  > value[count])min = value[count];
+	for(int j = 0; j < value[count].length; j++){
+		if(min > value[count][j])min = value[count][j];
 	}
-
+	}
 System.out.print(min);
 	}
 }
