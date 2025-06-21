afed3e31ddf26099bdadb8416a8bb93004a2d9db
rexx010 <grexx200@gmail.com>
2025-06-21 18:26:56 +0100

    added a balance inst variable to the account

--- java oop/AccountTest.java
+++ java oop/AccountTest.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 
 public class AccountTest{
-public static void main(String[] args){
+	public static void main(String[] args){
 
-Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-Account myAccount = new Account();
+		Account account1 = new Account("Peace John", 500.00);
+		Account account2 = new Account("Shawty Booty", -40.70);
 
-System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+		System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n%n", account2.getName(), account2.getBalance());
 
-System.out.println("please enter the name:");
+		System.out.print("Enter the deposit amount for account1: ");
+		double depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+		account1.deposit(depositAmount);
 
-String theName = input.nextLine();
-myAccount.setName(theName);
-System.out.println();
+		System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n%n", account2.getName(), account2.getBalance());
 
-System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+		System.out.print("Enter the deposit amount for account2: ");
+		depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account2 balance%n%n", depositAmount);
+		account2.deposit(depositAmount);
+
+		System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
+
+		System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
 
 }
 }
