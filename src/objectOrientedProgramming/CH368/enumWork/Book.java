ed38be3572bf1fff75c4a9e5d27f129733808c2e
rexx010 <grexx200@gmail.com>
2025-07-04 03:54:57 +0100

    add Book enum for managing programming book titles and copyright years

---
+++ src/objectOrientedProgramming/CH368/enumWork/Book.java
@@ -0,0 +1,24 @@
+package objectOrientedProgramming.CH368.enumWork;
+
+public enum Book {
+    JHTP("Java How to Program", "2018"),
+    CHTP("C, How to Program", "2016"),
+    IW3HTP("internet & World Wide Web How to Program", "2012"),
+    CPPHTP("C++ How to Program", "2017"),
+    VBHTP("Visual Basic How to Program", "2014"),
+    CSHARPHTP("Visual C# How to Program", "2017");
+
+    private final String title;
+    private final String copyrightYear;
+
+    Book(String title, String copyrightYear) {
+        this.title = title;
+        this.copyrightYear = copyrightYear;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getCopyrightYear() {
+        return copyrightYear;
+    }
+}
